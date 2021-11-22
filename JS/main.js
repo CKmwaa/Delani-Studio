52c86865a92812656d1367c02c55485b5c211745
Ckmwaa <caleb.mwangi@student.moringaschool.com>
2021-11-22 15:44:19 +0300

    Add jquery

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,26 @@
 // toogle  what to do icon
+$(document).ready(function(){
+    $("#image1").click(function(){
+        $("#design").show(2000)
+        $("#image1").hide(2000)
+        $("#design").addClass("alignDesign")
+        $("span").addClass("boldDesign")
+    })
+
+    $("#image2").click(function(){
+        $("#development").show(2000)
+        $("#image2").hide(2000)
+        $("#development").addClass("alignDesign")
+        $("span").addClass("boldDesign")
+    })
+    $("#image3").click(function(){
+        $("#product").show(2000)
+        $("#image3").hide(2000)
+        $("#product").addClass("alignDesign")
+        $("span").addClass("boldDesign")
+    })
+
+})
 
 // hover effect on portfolio
 
